143609ffa9c188a030bfd4ff994664069f708a3f
epuiiba <enricpuig26@gmail.com>
2022-10-31 10:59:51 +0100

    Mejora Crosshair, movimiento i cambio de color

---
+++ Assets/Scripts/Items/CubeRotation.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    public float velRot = 3.0f;
+    private float velx, vely, velz;
+    public float velU = 0.01f;
+    private float velUs, anguloGir;
+    public float offset = 0f;
+    private int[] ran = { -1, 1 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        velx = velRot * Random.Range(0.2f, 0.5f) * ran[Random.Range(0, 1)];
+        vely = velRot * Random.Range(0.2f, 0.5f) * ran[Random.Range(0, 1)];
+        velz = velRot * Random.Range(0.2f, 0.5f) * ran[Random.Range(0, 1)];
+        velUs = velU;
+        InvokeRepeating("ReRandomCube", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 rot = new Vector3(transform.rotation);s
+        //transform.Rotate(rot.x * velRot, rot.y, rot.x);
+        transform.Rotate(velx, vely, velz);
+        velUs = velU * Mathf.Sin((anguloGir * Mathf.PI) / 180);
+        anguloGir += 0.5f;
+        if(anguloGir==360) anguloGir = 0;
+        transform.Translate(0, velUs, 0, Space.World);
+        /*if(transform.position.y < 2 + offset)
+        {
+            velUs = velU;
+        }
+        else if(transform.position.y > 2.5 + offset) velUs = -velU; ;*/
+    }
+
+    void ReRandomCube()
+    {
+        velx = velRot * Random.Range(0.2f, 0.5f) * ran[Random.Range(0, 1)];
+        vely = velRot * Random.Range(0.2f, 0.5f) * ran[Random.Range(0, 1)];
+        velz = velRot * Random.Range(0.2f, 0.5f) * ran[Random.Range(0, 1)];
+    }
+}
---
+++ Assets/Scripts/Player/Crosshair.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public float velz = 3f;
+    public float velChange = 0.001f;
+    public float cX, cZ = 0f;   
+    public float cY = 1f;
+    private float multVel;
+    private float anguloGir = 0f;
+    private float anguloGir2 = 180f;
+    private UnityEngine.UI.Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cY = 0.9f;
+        image = this.GetComponent<UnityEngine.UI.Image>();
+        image.color = new Color(0f, 0.9f, 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f,0f,velz);
+        UpdateRGB();
+        UpdateColor();
+
+        
+    }
+
+    private void UpdateColor()
+    {
+        image.color = new Color(cX, cY, cZ, 1f);
+
+    }
+
+    private void UpdateRGB()
+    {
+        float a = 0.5f * Mathf.Sin((anguloGir * Mathf.PI) / 180) + 0.5f;
+        float b = 0.5f * Mathf.Sin(((anguloGir+180f) * Mathf.PI) / 180) + 0.5f;
+        if(a >= 0.30f && a <= 0.60f) multVel = 10f;
+        else multVel = 1;
+        anguloGir += velChange * multVel;
+        if(anguloGir > 360) anguloGir -= 360;
+        cX = a;
+        cZ = a;
+        cY = b;
+    }
+
+}
