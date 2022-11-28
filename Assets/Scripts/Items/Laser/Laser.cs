9378a1792a899792404632f43a6e26c580d179b7
epuiiba <enricpuig26@gmail.com>
2022-11-28 11:58:29 +0100

    Laser

--- Assets/Scripts/Items/Laser/Laser.cs
+++ Assets/Scripts/Items/Laser/Laser.cs
@@ -10,26 +10,43 @@ public class Laser : MonoBehaviour
     public Vector3 direction;
 
     private int lMask;
+
+    public GameObject LaserPrefab;
+    private GameObject OtherLaser;
+    private Laser OtherLaserScript;
+    public bool summoning;
+
     // Start is called before the first frame update
     void Start()
     {
+        summoning = false;
         lr = GetComponent<LineRenderer>();
-        lMask = (1 << 20);
+        lMask = 1 << 20;
         lMask = ~lMask;
+        startPoint = transform.position;
+        direction = Vector3.up;
     }
-
-    public void Init(Vector3 p, Vector3 d)
+    public void setSum(bool b)
     {
-        
+        summoning = b;
+    }
+    /*public void OnEnable()
+    {
         lr = GetComponent<LineRenderer>();
         lMask = (1 << 20);
         lMask = ~lMask;
-    }
-    // Update is called once per frame
-    void UpdateLaser(Vector3 p, Vector3 d)
+        startPoint = transform.position;
+        direction = Vector3.up;
+    }*/
+    public void setPosAndDir(Vector3 p, Vector3 d)
     {
         startPoint = p;
         direction = d;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
         lr.SetPosition(0, startPoint);
         RaycastHit hit;
         if(Physics.Raycast(startPoint, direction, out hit, 6000, lMask))
@@ -38,11 +55,34 @@ public class Laser : MonoBehaviour
             {
                 lr.SetPosition(1, hit.point);
             }
+
             if(hit.transform.tag == "Portal")
             {
-                PortalScript ps = hit.transform.gameObject.GetComponent<PortalScript>();
+                GameObject portal = hit.transform.gameObject;
+                PortalScript portalScript = portal.GetComponent<PortalScript>();
+                Vector3 LocalPos = portal.transform.InverseTransformPoint(hit.point);
+                Vector3 OtherPos = portalScript.OtherPortal.transform.TransformPoint(-new Vector3(LocalPos.x, -LocalPos.y, LocalPos.z));
+
+                Vector3 LocalDir = portal.transform.InverseTransformDirection(direction);
+                Vector3 OtherDir = portalScript.OtherPortal.transform.TransformDirection(-new Vector3(LocalPos.x, -LocalPos.y, LocalPos.z));
+                OtherDir = Quaternion.FromToRotation(portal.transform.rotation, portalScript.OtherPortal.transform.rotation);
+                if(OtherLaser == null)
+                {
+                    OtherLaser = Instantiate(LaserPrefab, transform.position, transform.rotation, transform);
+                    OtherLaserScript = OtherLaser.GetComponent<Laser>();
+                    summoning = true;
+                }
+                Debug.Log(OtherLaser);
+                OtherLaserScript.setPosAndDir(OtherPos, OtherDir);
             }
+            else //if(OtherLaser != null)
+            {
+                Destroy(OtherLaser);
+                OtherLaserScript = null;
+                summoning = false;
+            }
+
         }
-        else lr.SetPosition(1, transform.forward * 5000);
+        else lr.SetPosition(1, direction * 5000);
     }
 }
--- Assets/Scripts/Items/Laser/LaserThrower.cs
+++ Assets/Scripts/Items/Laser/LaserThrower.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LaserThrower : MonoBehaviour
 {
+    public GameObject Laser;
+    private Laser LaserScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject LaserS = Instantiate(Laser, transform.position, transform.rotation, transform);
+        Debug.Log("a "+ LaserS);
+        LaserScript = LaserS.GetComponent<Laser>();
+        Debug.Log("b "+ LaserScript);
+        LaserScript.setPosAndDir(transform.position, transform.up);
+        LaserScript.setSum(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LaserScript.setPosAndDir(transform.position, transform.forward);
     }
 }
