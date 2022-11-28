ec55ba140968b0fef6cc87c9fa31a08364379eb1
Quique <quiquegallifa01@gmail.com>
2022-11-28 11:04:00 +0100

    añadido diálogo a la piedra del nivel 2

--- Assets/Scripts/Niveles/PiedraNivel2.cs
+++ Assets/Scripts/Niveles/PiedraNivel2.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PiedraNivel2 : Interactable
 {
     FPSController player;
-    GameObject panelImagen;
+    GameObject PilarPiedra;
+    GameObject contenedorTexto;
+    GameObject historia;
+    TMP_Text texto;
     // Start is called before the first frame update
     void Start()
     {   
         player = GameObject.Find("Mage").GetComponent<FPSController>();
-        panelImagen = transform.Find("DialogoPiedra").gameObject;
+        PilarPiedra = GameObject.Find("PilarAux");
+        //panelImagen = transform.Find("PilarPiedra/DialogoPiedra");
+
+        historia = PilarPiedra.transform.Find("ParaDialogo/Historia").gameObject;
+        contenedorTexto = PilarPiedra.transform.Find("ParaDialogo/Historia/Textito").gameObject;
+        texto = contenedorTexto.GetComponent<TMP_Text>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return) && historia.activeSelf)
+        {
+            historia.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -19,8 +36,10 @@ public class PiedraNivel2 : Interactable
         //Debug.Log ("Portal Triggering Mal");
         player.canShoot1 = true;
         player.canShoot2 = true;
-
-        
-        panelImagen.SetActive(true);
+        if (historia != null)
+        {
+            historia.SetActive(true);
+            texto.SetText("ALELUYA");
+        }
     }
 }
